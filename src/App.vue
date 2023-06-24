1ed8ad13269333c90d65e5defd15af28d16605ef
Mirabbosbek <mirabbosabduvohidov1@gmail.com>
2023-06-24 19:42:10 +0500

    first lesson configuration

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Counter:{{ counter }}</h1>
+    <div class="d-flex">
+      <div @click="onIncreament" class="btn btn-success">ADD</div>
+      <div @click="onDecrement" class="btn btn-danger">Delete</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    onIncreament() {
+      this.counter += 1;
+    },
+    onDecrement() {
+      this.counter -= 1;
+    },
+  },
+};
+</script>
+
+<style></style>
+ 
