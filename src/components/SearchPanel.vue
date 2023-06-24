1b95b429a8dbc02c70c5230a4e322596c73c66cc
Mirabbosbek <mirabbosabduvohidov1@gmail.com>
2023-06-24 22:42:16 +0500

    configutation project 2

---
+++ src/components/SearchPanel.vue
@@ -0,0 +1,21 @@
+<template>
+  <input
+    type="text"
+    class="form-control search-input"
+    placeholder="Kinolarni qidirish"
+  />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.search-input { 
+  margin-bottom: 1rem;
+  padding: 1.5rem;
+  background-color: rgb(194, 165, 165);
+  border-radius: 4px;
+  box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.15);
+}
+</style>
---
+++ src/components/app/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="app font-monospace">
+    <div class="content">
+      <AppInfo />
+      <div class="search-panel">
+        <SearchPanel />
+        <AppFilter />
+    </div>
+        <MoveList />
+        <MoveAddForm/>
+    </div> 
+  </div>
+</template>
+
+<script>
+import AppInfo from "../app-info/AppInfo.vue";
+import SearchPanel from "../SearchPanel.vue";
+import AppFilter from "../app-filter/AppFilter.vue";
+import MoveList from "../move-list/MoveList.vue";
+import MoveAddForm from "../move-add-form/MoveAddForm.vue";
+
+export default {
+  components: {
+    AppInfo,
+    SearchPanel, 
+    AppFilter,
+    MoveList,
+    MoveAddForm,
+  },
+};  
+</script>
+
+<style>
+.app {
+  height: 100vh;
+  color: black;
+}
+.content {
+  width: 1000px;
+  min-height: 700px;
+  background-color: white;
+  margin: 0 auto;
+  padding: 5rem 0;
+}
+.search-panel {
+  margin-top: 2rem;
+  padding: 1.5rem;
+  background-color: white;
+  border-radius: 4px;
+  box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.15);    
+}
+</style>
