d97c7e0203025c8935a73ea8f22d20db7b510ccd
Mirabbosbek <mirabbosabduvohidov1@gmail.com>
2023-06-28 02:29:40 +0500

    unique key,Date.now(),new key

--- src/components/move-add-form/MoveAddForm.vue
+++ src/components/move-add-form/MoveAddForm.vue
@@ -38,6 +38,7 @@ export default {
         viewers: this.viewers,
         favourite: false,
         like: false,
+        id:Date.now()
       };
       this.$emit("createMovie", newMovie);
       this.name = "";
